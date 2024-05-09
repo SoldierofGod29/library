6183b74a3294cd8f6be5e5794e4ea3448277ba41
SoldierofGod29 <nzollers@hotmail.com>
2024-05-09 17:43:59 -0500

    Fix shelf in style sheet to display the width of open shelf area; Fix bugs with deleteBookFromLibrary and displayBooksInDocument when adding shelf; Add function checkNumBooksOnShelf to check the windows width and return number of books that can fit on the shelf

--- javascript.js
+++ javascript.js
@@ -3,7 +3,9 @@
 /*Document and Media Queries for Library Project*/
 
 //Variables for different widths of the window
-const maxWidth1440px = window.matchMedia("(max-width: 1440px)");
+const windowWidth = window.screen.width;
+
+const maxWidth320 = window.matchMedia('(max-width: 320px)');
 
 //Buttons
 const addBookButton = document.querySelector(".add-book");
@@ -26,6 +28,7 @@ const myLibrary = [];
 let totalBooksInLibrary = 0;
 let totalShelvesInLibrary = 0;
 
+console.log(windowWidth);
 /*Functions for Library Project*/
 
 //Book constructor for holding Book info
@@ -34,6 +37,59 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
+}
+
+function checkNumBooksOnShelf()
+{
+    let numBooksOnShelf;
+
+    switch (true)
+    {
+        case (windowWidth > 0 && windowWidth < 990):
+            numBooksOnShelf = 5;
+            break;
+        case (windowWidth > 989 && windowWidth < 1125):
+            numBooksOnShelf = 6;
+            break;
+        case (windowWidth > 1124 && windowWidth < 1260):
+            numBooksOnShelf = 7;
+            break;
+        case (windowWidth > 1259 && windowWidth < 1395):
+            numBooksOnShelf = 8;
+            break;
+        case (windowWidth > 1394 && windowWidth < 1530):
+            numBooksOnShelf = 9;
+            break;
+        case (windowWidth > 1529 && windowWidth < 1665):
+            numBooksOnShelf = 10;
+            break;
+        case (windowWidth > 1664 && windowWidth < 1800):
+            numBooksOnShelf = 11;
+            break;
+        case (windowWidth > 1799 && windowWidth < 1935):
+            numBooksOnShelf = 12;
+            break;
+        case (windowWidth > 1934 && windowWidth < 2070):
+            numBooksOnShelf = 13;
+            break;
+        case (windowWidth > 2069 && windowWidth < 2205):
+            numBooksOnShelf = 14;
+            break;
+        case (windowWidth > 2204 && windowWidth < 2340):
+            numBooksOnShelf = 15;
+            break;
+        case (windowWidth > 2339 && windowWidth < 2476):
+            numBooksOnShelf = 16;
+            break;
+        case (windowWidth > 2475 && windowWidth < 2610):
+            numBooksOnShelf = 17;
+            break;
+        default:
+            numBooksOnShelf = 18;
+    }
+
+    console.log(numBooksOnShelf);
+    return numBooksOnShelf;
 }
 
 //Function to add a book to the myLibrary Array
@@ -67,7 +123,7 @@ function deleteBookFromLibrary(bookid, shelfid)
     if (confirm(warning) == true)
     {
         myLibrary.splice(bookid, 1);
-        if (bookid == 0 || bookid % 9 == 0)
+        if (myLibrary.length == 0 || myLibrary.length % checkNumBooksOnShelf() == 0)
         {
             const removeShelf = document.querySelector('#s' + shelfid);
             removeShelf.remove();
@@ -76,6 +132,7 @@ function deleteBookFromLibrary(bookid, shelfid)
         }
 
         totalBooksInLibrary--;
+
         displayAfterDeletion();       
     }
 }
@@ -125,11 +182,11 @@ function displayAfterDeletion()
 }
 
 //Function to loop through myLibrary Array and display it in the DOM
-function displayBooksInDocument()
+function displayBooksInDocument(numBooks)
 {
     deleteAllOfLibrary();
 
-    if (totalBooksInLibrary == 0 || totalBooksInLibrary % 9 == 0)
+    if (totalBooksInLibrary % numBooks == 0)
     {
         totalShelvesInLibrary++;
     }
@@ -173,9 +230,10 @@ function displayBooksInDocument()
         newDelButton.setAttribute('onclick', "deleteBookFromLibrary(" + i + ", " + totalShelvesInLibrary + ")");
     }
 
-    if (totalBooksInLibrary == 0 || totalBooksInLibrary % 9 == 0)
+    if (totalBooksInLibrary == 0 || totalBooksInLibrary % numBooks == 0)
     {
         let lastBook = document.querySelector('#book' + totalBooksInLibrary);
+
         let positionOfLastBook = lastBook.offsetTop;
 
         const newShelf = document.createElement('div');
@@ -195,10 +253,6 @@ function displayBooksInDocument()
 
 
 /*Event Listeners for Library Project*/
-
-maxWidth1440px.addEventListener("change", function() {
-    
-});
 
 addBookButton.addEventListener("click", function() {
     dialog.showModal();
@@ -223,7 +277,7 @@ submitForm.addEventListener("click", function(event) {
 
     addBookToLibrary(title.value, author.value, pages.value, readStatus);
 
-    displayBooksInDocument();
+    displayBooksInDocument(checkNumBooksOnShelf());
 
     totalBooksInLibrary++;
 
